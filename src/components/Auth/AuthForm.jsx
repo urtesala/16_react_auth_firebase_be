2f78cd39a6053c1771d0bc9d68d9283db5be60e9
urtesala <urtesaladziute@gmail.com>
2022-12-13 09:41:18 +0300

    install formik, control form

--- src/components/Auth/AuthForm.jsx
+++ src/components/Auth/AuthForm.jsx
@@ -1,30 +1,56 @@
+import { useFormik } from 'formik';
 import { useState } from 'react';
 
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
-  const [isLogin, setIsLogin] = useState(true);
+  const [isLogin, setIsLogin] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    onSubmit: (values) => {
+
+      // registred or login?
+      const regOrLogin = isLogin ? 'login':'register'
+      console.log('values ===', values, 'mode', regOrLogin);
+    },
+  });
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={formik.handleSubmit}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input
+            type='email'
+            id='email'
+            required
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input
+            type='password'
+            id='password'
+            required
+            onChange={formik.handleChange}
+            value={formik.values.password}
+          />
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? 'Login' : 'Create Account'}</button>
           <button
-            type='button'
+            type='submit'
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
